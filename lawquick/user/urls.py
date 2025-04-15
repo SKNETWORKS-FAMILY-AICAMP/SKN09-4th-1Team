3981d21e5986b57d766a6e58fc273e453773e9ba
nini12091 <nini12091@naver.com>
2025-04-15 19:35:02 +0900

    feat: password page (#7)

--- lawquick/user/urls.py
+++ lawquick/user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'user'
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('password/', views.find_password, name='find_password'),
 ]
--- lawquick/user/views.py
+++ lawquick/user/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'user/home_01.html')
+
+def find_password(request):
+    return render(request, 'user/search_01.html')
