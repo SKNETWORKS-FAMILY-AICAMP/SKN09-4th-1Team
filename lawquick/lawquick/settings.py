8e916dc3dd3d1202bd02786834245daa2173c316
nini12091 <nini12091@naver.com>
2025-04-15 18:38:50 +0900

    fix: code remove (#3)

--- lawquick/lawquick/settings.py
+++ lawquick/lawquick/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)@llu7t((qgiju^d!1x941!0=)9cj1x@-ock2l--jq$-io!^p^'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
