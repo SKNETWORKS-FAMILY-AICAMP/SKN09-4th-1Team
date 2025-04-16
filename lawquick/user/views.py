b260ef16ba081ef66233d4c586b75fc1d236e800
nini12091 <nini12091@naver.com>
2025-04-16 17:53:31 +0900

    feat: db migrations (#14)

--- lawquick/user/views.py
+++ lawquick/user/views.py
@@ -1,18 +1,47 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .services.auth_service import authenticate_user
+from .repositories.user_repository import user_exists_by_email, get_user_by_email
 
 def home(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate_user(email, password)
+        if user:
+            request.session['user_id'] = str(user.id)
+            
+            # ✅ [임시 코드] 세션 기반 로그인 확인 메시지 (향후 삭제 예정)
+            ######## ###### ###### ###### ###### ###### ###### ###### 
+            request.session['user_email'] = user.email
+            ####### ####### ###### ###### ###### ###### ###### ######
+
+            return redirect('user:home')
+        else:
+            messages.error(request, '이메일 또는 비밀번호가 올바르지 않습니다.')
+
     return render(request, 'user/home_01.html')
+
 
 def find_password(request):
     if request.method == 'POST':
-        return redirect('user:find_password_email_confirm')
+        email = request.POST.get('email')
+        if user_exists_by_email(email):
+            request.session['reset_email'] = email
+            return redirect('user:find_password_complete')
+        else:
+            messages.error(request, '입력한 이메일 주소를 찾을 수 없습니다.')
     return render(request, 'user/search_01.html')
 
 
-def find_password_email_confirm(request):
-    if request.method == 'POST':
-        return redirect('user:find_password_complete')
-    return render(request, 'user/search_02.html')
-
 def find_password_complete(request):
-    return render(request, 'user/search_03.html')
+    email = request.session.get('reset_email')
+    user = get_user_by_email(email)
+    return render(request, 'user/search_02.html', {'user': user})
+
+
+def logout_view(request):
+    request.session.flush()
+    messages.info(request, "로그아웃 되었습니다.")
+    return redirect('user:home')
