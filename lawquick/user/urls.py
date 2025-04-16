6fdeac5a91630573f9bbd11e221100b6a6fe57e2
nini12091 <nini12091@naver.com>
2025-04-16 09:28:39 +0900

    FEAT: search02_03 page (#12)

--- lawquick/user/urls.py
+++ lawquick/user/urls.py
@@ -6,4 +6,6 @@ app_name = 'user'
 urlpatterns = [
     path('', views.home, name='home'),
     path('password/', views.find_password, name='find_password'),
+    path('password/confirm/', views.find_password_email_confirm, name='find_password_email_confirm'),
+    path('password/complete/', views.find_password_complete, name='find_password_complete'),
 ]
