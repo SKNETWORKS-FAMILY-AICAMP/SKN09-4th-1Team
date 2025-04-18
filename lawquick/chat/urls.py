917a4488700c5c983cd95a72471308d77e9f4d20
YUJINDL01 <qkrdbwlsdl01@gmail.com>
2025-04-18 20:33:58 +0900

    guest and db_chat and db_message connection

--- lawquick/chat/urls.py
+++ lawquick/chat/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'chat'  # 네임스페이스 설정
+
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    # 채팅 시작 화면 (게스트, 멤버 선택)
-    path('chat/guest/00/', views.chat_guest_start, name='chat_guest_start'),
-    path('chat/member/00/', views.chat_member_start, name='chat_member_start'),
-    
-    # 게스트/멤버 채팅 화면 (각각의 채팅 화면)
-    path('chat/guest/01/', views.chat_guest, name='chat_guest'),
+    path('', views.chat_entry, name='chat_entry'),
+
+    # 게스트 채팅 플로우
+    path('guest/00/', views.chat_guest_start, name='chat_guest_start'),  # 시작화면 (입력폼)
+    path('chat/guest/01/', views.chat_guest_save, name='chat_guest_save'),  # 질문 저장
+    path('guest/chat/<int:chat_id>/', views.chat_guest, name='guest_chat_with_id'),  # 채팅화면
+
+    # 멤버 채팅
+    path('member/00/', views.chat_member_start, name='member_start'),
+    # 추후 멤버도 저장 플로우 추가
     path('chat/member/01/', views.chat_member, name='chat_member'),
 ]
