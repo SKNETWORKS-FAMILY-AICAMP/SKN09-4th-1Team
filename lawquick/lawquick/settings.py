ff8b5dae2d01a2638d323f55184c8147ae495632
YUJINDL01 <qkrdbwlsdl01@gmail.com>
2025-04-21 18:51:31 +0900

    doker_setting

--- lawquick/lawquick/settings.py
+++ lawquick/lawquick/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'lawquick.com']
 
 
 # Application definition
